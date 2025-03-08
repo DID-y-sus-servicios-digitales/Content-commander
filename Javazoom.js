5ea236fbfe85533053a80367393a02c6d7a0164d
DID-y-sus-servicios-digitales <yutencurso@gmail.com>
2025-03-08 10:48:31 +0100

    Update Javazoom.js

--- Javazoom.js
+++ Javazoom.js
@@ -1,15 +1,15 @@
-cconst image = document.getElementById('img');
+const image = document.getElementById('img');
 const zoomInButton = document.getElementById('zoomIn');
 const zoomOutButton = document.getElementById('zoomOut');
 
-let currentWidth = 16; // Ancho inicial de la imagen
+let currentWidth = 16; // Ancho inicial de la imagen (ajusta este valor)
 
 zoomInButton.addEventListener('click', () => {
-  currentWidth += 600; // Aumentar el ancho en 20 píxeles
+  currentWidth += 20; // Aumentar el ancho en 20 píxeles
   image.style.width = `${currentWidth}px`;
 });
 
 zoomOutButton.addEventListener('click', () => {
-  currentWidth -= 600; // Reducir el ancho en 20 píxeles
+  currentWidth -= 20; // Reducir el ancho en 20 píxeles
   image.style.width = `${currentWidth}px`;
 });
